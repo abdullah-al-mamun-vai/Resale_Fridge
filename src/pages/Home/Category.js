54ba236330af12e7c1e9576e275aa1d21aae30b2
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-11-25 20:11:35 +0100

    react query added

--- src/pages/Home/Category.js
+++ src/pages/Home/Category.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Category = () => {
     return (
-        <div>
+        <div className='py-24'>
+            <h2 className='capitalize font-bold text-4xl my-5'>Category</h2>
 
         </div>
     );
